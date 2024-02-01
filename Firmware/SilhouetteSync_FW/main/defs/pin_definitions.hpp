8fe832a507264431ae64d8b731092768ce9044b7
myles-parfeniuk <myles.parfenyuk@gmail.com>
2024-02-01 01:05:20 -0800

    esp32C3 port

--- Firmware/SilhouetteSync_FW/main/defs/pin_definitions.hpp
+++ Firmware/SilhouetteSync_FW/main/defs/pin_definitions.hpp
@@ -2,10 +2,9 @@
 #include "driver/gpio.h"
 
 //IMU (SPI)
-static constexpr gpio_num_t pin_imu_hint = GPIO_NUM_26; ///<IMU host interrupt pin
-static constexpr gpio_num_t pin_imu_rst = GPIO_NUM_32; ///<IMU reset pin
-static constexpr gpio_num_t pin_imu_cs = GPIO_NUM_33; ///<IMU SPI chip select pin
+static constexpr gpio_num_t pin_imu_hint = GPIO_NUM_6; ///<IMU host interrupt pin
+static constexpr gpio_num_t pin_imu_rst = GPIO_NUM_7; ///<IMU reset pin
+static constexpr gpio_num_t pin_imu_cs = GPIO_NUM_5; ///<IMU SPI chip select pin
 static constexpr gpio_num_t pin_imu_scl = GPIO_NUM_18; ///<IMU SPI SCL pin
 static constexpr gpio_num_t pin_imu_sda = GPIO_NUM_19; ///<IMU SDA pin (esp32 MISO pin)
-static constexpr gpio_num_t pin_imu_di = GPIO_NUM_23; ///<IMU DI pin (esp32 MOSI pin)
-
+static constexpr gpio_num_t pin_imu_di = GPIO_NUM_4; ///<IMU DI pin (esp32 MOSI pin)
