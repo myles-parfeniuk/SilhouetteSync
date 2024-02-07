75b69797dc53dd1b869015eefe566c3142dcbf95
Kieran MacCarron <146384447+kmac333@users.noreply.github.com>
2024-02-07 14:18:57 -0800

    UnityApp1.02

--- Software/UnityApp/SilSync/Assets/Code/wireless.cs
+++ Software/UnityApp/SilSync/Assets/Code/wireless.cs
@@ -3,9 +3,12 @@ using System;
 using System.Net.Sockets;
 using System.Threading;
 using System.Runtime.InteropServices;
+using UnityEngine.UI;
+using TMPro;
 
 public class IMUController : MonoBehaviour
 {
+    //public static IMUController instance;
     [System.Serializable]
     public struct Payload
     {
@@ -15,14 +18,38 @@ public class IMUController : MonoBehaviour
         public float z_heading;
         public byte accuracy;
     }
+    public Transform limb1;
+    public Transform limb2;
+    /*
+    public Transform limb3;
+    public Transform limb4;
+    public Transform limb5;
+    */
 
-    private TcpClient imuTcpClient;
-    private NetworkStream imuNetworkStream;
+    public TcpClient imuTcpClient1;
+    public TcpClient imuTcpClient2;
+    private NetworkStream imuNetworkStream1;
+    private NetworkStream imuNetworkStream2;
     private Thread imuThread;
     private bool isRunning = true;
 
-    private Quaternion position;
+    private Quaternion position1;
+    private Quaternion position2;
+    /*
+    private Quaternion position3;
+    private Quaternion position4;
+    private Quaternion position5;
+    
 
+    void Awake() {
+        if(instance != null && instance != this) {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    */
 
     void Start()
     {
@@ -32,28 +59,42 @@ public class IMUController : MonoBehaviour
 
     void Update()
     {
-        transform.rotation = position;
+        limb1.transform.rotation = position1;
+        limb2.transform.rotation = position2;
+        /*
+        limb3.rotation = position1;
+        limb4.rotation = position1;
+        limb5.rotation = position1;
+        */
     }
 
     void OnApplicationQuit()
     {
         isRunning = false;
 
-        if (imuNetworkStream != null)
-            imuNetworkStream.Close();
+        if (imuNetworkStream1 != null)
+            imuNetworkStream1.Close();
 
-        if (imuTcpClient != null)
-            imuTcpClient.Close();
+        if (imuTcpClient1 != null)
+            imuTcpClient1.Close();
+
+        if (imuNetworkStream2 != null)
+            imuNetworkStream2.Close();
+
+        if (imuTcpClient2 != null)
+            imuTcpClient2.Close();
     }
 
     void ReceiveData()
     {
         try
         {
-            imuTcpClient = new TcpClient("192.168.158.50", 49160);
-            imuNetworkStream = imuTcpClient.GetStream();
+            imuTcpClient1 = new TcpClient("192.168.187.50", 49160);
+            imuNetworkStream1 = imuTcpClient1.GetStream();
+            imuTcpClient2 = new TcpClient("192.168.187.65", 49160);
+            imuNetworkStream2 = imuTcpClient2.GetStream();
 
-            long prevTime = 0;
+            //long prevTime = 0;
 
             while (isRunning)
             {
@@ -62,11 +103,11 @@ public class IMUController : MonoBehaviour
                 // Convert struct to byte array
                 byte[] payloadBytes = StructToBytes(payloadOut);
 
-                imuNetworkStream.Write(payloadBytes, 0, payloadBytes.Length);
+                imuNetworkStream1.Write(payloadBytes, 0, payloadBytes.Length);
                 Debug.Log("Sent " + payloadBytes.Length + " bytes");
 
                 byte[] buffer = new byte[Marshal.SizeOf<Payload>()]; // Use Marshal.SizeOf to get the size of the struct
-                int bytesRead = imuNetworkStream.Read(buffer, 0, buffer.Length);
+                int bytesRead = imuNetworkStream1.Read(buffer, 0, buffer.Length);
 
                 // Convert byte array to struct
                 Payload payloadIn = BytesToStruct<Payload>(buffer);
@@ -78,14 +119,37 @@ public class IMUController : MonoBehaviour
                         ", Y_heading=" + y.ToString("F4") + ", Z_Heading=" + z.ToString("F4") +
                          ", Accuracy=" + payloadIn.accuracy);
 
-                position = Quaternion.Euler(x, y, z);
+                position1 = Quaternion.Euler(x, y, z);
 
-                long currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                payloadOut = new Payload { id = 0, x_heading = 0 };
+
+                // Convert struct to byte array
+                payloadBytes = StructToBytes(payloadOut);
+
+                imuNetworkStream2.Write(payloadBytes, 0, payloadBytes.Length);
+                Debug.Log("Sent " + payloadBytes.Length + " bytes");
+
+                buffer = new byte[Marshal.SizeOf<Payload>()]; // Use Marshal.SizeOf to get the size of the struct
+                bytesRead = imuNetworkStream2.Read(buffer, 0, buffer.Length);
+
+                // Convert byte array to struct
+                payloadIn = BytesToStruct<Payload>(buffer);
+                x = payloadIn.x_heading  * 180 / Mathf.PI;
+                y =  payloadIn.y_heading * 180 / Mathf.PI;
+                z = payloadIn.z_heading  * 180 / Mathf.PI;
+                
+                Debug.Log("Received id=" + payloadIn.id + ", X_heading=" + x.ToString("F4") +
+                        ", Y_heading=" + y.ToString("F4") + ", Z_Heading=" + z.ToString("F4") +
+                         ", Accuracy=" + payloadIn.accuracy);
+
+                position2 = Quaternion.Euler(x, y, z);
+
+                //long currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
                 //Debug.Log("Received id=" + payloadIn.id + ", X_heading=" + payloadIn.x_heading.ToString("F4") +
                 //         ", Y_heading=" + payloadIn.y_heading.ToString("F4") + ", Z_Heading=" + payloadIn.z_heading.ToString("F4") +
                 //          ", Accuracy=" + payloadIn.accuracy + ", TIMESTAMP: " + ((currentTime - prevTime) / 1000.0).ToString("F4"));
 
-                prevTime = currentTime;
+                //prevTime = currentTime;
 
                 // Add a delay to avoid flooding the server with requests
                 //Thread.Sleep(10);
@@ -93,7 +157,7 @@ public class IMUController : MonoBehaviour
         }
         catch (Exception e)
         {
-            Debug.LogError("Error: " + e.Message);
+            Debug.Log("Error: " + e.Message);
         }
     }
 
