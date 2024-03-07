663b2f4e418ef721f89d3223397cbe4888410655
myles-parfeniuk <myles.parfenyuk@gmail.com>
2024-03-06 19:07:29 -0800

    Updated testing scripts

--- TestingScripts/pyClientTest/UDPMultiThreadedMultiSocketServer/main.py
+++ TestingScripts/pyClientTest/UDPMultiThreadedMultiSocketServer/main.py
@@ -21,30 +21,26 @@ server_adresses = [
 prev_times = [0] * len(server_adresses)
 data_streams = []
 quit_flag = threading.Event() 
+client_threads = []
 
-def client_thread():
-    global data_streams
-    global server_adresses
-    global prev_times 
+def client_thread(data_stream, server_address, prev_time):
     global quit_flag
 
     while not quit_flag.is_set():
+        try:
+            #request/receieve sample from device i
+            payload_out, payload_in = send_sample_req(data_stream, server_address)
 
-            i = 0
-            for server_address in server_adresses:
-                try:
-                    #request/receieve sample from device i
-                    payload_out, payload_in = send_sample_req(data_streams[i], server_address)
+            # print the received data from device i
+            print_packet(payload_in, payload_out, payload_in.time_stamp, prev_time)
+            #print(str(prev_times[i]) + " " + str(payload_in.time_stamp) + '\n')
+            prev_time = payload_in.time_stamp
 
-                    # print the received data from device i
-                    print_packet(payload_in, payload_out, payload_in.time_stamp, prev_times[i])
-                    #print(str(prev_times[i]) + " " + str(payload_in.time_stamp) + '\n')
-                    prev_times[i] = payload_in.time_stamp
-                    i += 1
+        except socket.timeout:
+            print(colored('ERROR: Timeout occurred, resending request...', 'red'))
+            continue
 
-                except socket.timeout:
-                    print(colored('ERROR: Timeout occurred, resending request...', 'red'))
-                    continue
+    data_stream.close()
 
 def keyboard_input_thread():
     global quit_flag
@@ -57,19 +53,28 @@ def keyboard_input_thread():
             quit_flag.set()
 
 
+
+#initialize colorama
+init()
+
 #create sockets
 for address in server_adresses:
     data_streams.append(socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
     data_streams[-1].settimeout(0.015)  # 15 milliseconds
 
+#create threads
+for i, address in enumerate(server_adresses):
+    client_threads.append(threading.Thread(target=client_thread, args=(data_streams[i], address, prev_times[i])))
+
+keyboard_input_thread_hdl = threading.Thread(target = keyboard_input_thread)
+
+
 #initialize colorama
 init()
 
+for thread in client_threads:
+    thread.start()
 
-client_thread_hdl = threading.Thread(target = client_thread) 
-keyboard_input_thread_hdl = threading.Thread(target = keyboard_input_thread)
-
-client_thread_hdl.start()
 keyboard_input_thread_hdl.start()
 
 # grab data until 'q' key is hit
@@ -79,10 +84,9 @@ while not quit_flag.is_set():
 
 
 print(colored('Quitting, socket shutting down.', 'magenta'))
-for data_stream in data_streams:
-    data_stream.close()
+for thread in client_threads:
+    thread.join()
 
-client_thread_hdl.join()
 keyboard_input_thread_hdl.join()
 
      
