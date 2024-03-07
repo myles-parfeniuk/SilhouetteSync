0961d3b8d0d5ee62f5f9a40c73c849f82bde7d9e
myles-parfeniuk <myles.parfenyuk@gmail.com>
2024-03-06 19:09:11 -0800

    Updated testing scripts

--- TestingScripts/pyClientTest/UDPSingleServerCommandTesting/main.py
+++ TestingScripts/pyClientTest/UDPSingleServerCommandTesting/main.py
@@ -1,42 +1,22 @@
+# Third-party and standard packages
 import socket
 import time
-import keyboard  # Import the keyboard module
+import threading
+import keyboard
+import numpy as np  # Import NumPy for array manipulation
 from ctypes import *
 from enum import Enum
-import sys
 import colorama
 from colorama import init
 from termcolor import colored
-from prettytable import PrettyTable
+import matplotlib.pyplot as plt  # Importing Matplotlib for plotting
 
-class Requests(Enum):
-    SAMPLE_REQ = 0
-    TARE_REQ = 1
-    CALIBRATE_REQ = 2
-
-class Responses(Enum):
-    BUSY_RESP = 255
-    SUCESS_RESP = 254
-    FAILURE_RESP = 253
-    AFFIRMATIVE_RESP = 252
-    NULL_RESP = 251
-    SAMPLING_RESP = 0
-# payload struct, how data is formatted when sent between devices
-class Payload(Structure):
-    _fields_ = [("request", c_uint8),
-                ("response", c_uint8),
-                ("id", c_uint64),
-                ("quat_i", c_float),
-                ("quat_j", c_float),
-                ("quat_k", c_float),
-                ("quat_real", c_float),
-                ("accuracy", c_uint8),
-                ("time_stamp", c_uint64)
-                ]
-
+# In-house packages
+from UDPClient import *
+from PacketTransceiver import print_packet
 
 # Host and port of the ESP32 UDPServer
-server_address_1 = ('192.168.1.96', 49160)
+server_address = ('192.168.1.91', 49160)
 
 # Create a UDP socket
 data_stream = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -44,143 +24,108 @@ data_stream = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Set timeout for receiving data
 data_stream.settimeout(0.015)  # 15 milliseconds
 
+quit_flag = threading.Event() 
+send_tare_flag = threading.Event()
+send_calibration_flag = threading.Event()
 
-# use Colorama to make Termcolor work on Windows too
+
+# Use Colorama to make Termcolor work on Windows too
 init()
 
-def send_packet(request, response,  server_address):
-    global data_stream
-    payload_out = Payload(0, 0)
-    payload_out.request = request
-    payload_out.response = response
-    data_stream.sendto(payload_out, server_address)
-    return payload_out
-
-def receive_packet():
-    global data_stream
-    payload_in = Payload(0, 0)
-
-    buff, _ = data_stream.recvfrom(sizeof(Payload))
-
-    if len(buff) != sizeof(Payload):
-        print(colored('ERROR: Received buffer size does not match expected payload size.', 'red'))
-
-    payload_in = Payload.from_buffer_copy(buff)
-
-    return payload_in
-
-def print_packet(payload_in, payload_out, current_time, prev_time):
-
-    packet_table = PrettyTable(['Packet Direction', 'Timestamp (ms)', 'Request', 'Response', 'ID', 'Quat I', 'Quat J', 'Quat K', 'Quat Real', 'Accuracy'])
-
-    timestamp = "N/A"
-    request = Requests(payload_out.request).name
-    response = Responses(payload_out.response).name
-    id_value = "0x{:X}".format(payload_out.id)
-    quat_i = "{:.4f}".format(payload_out.quat_i)
-    quat_j = "{:.4f}".format(payload_out.quat_j)
-    quat_k = "{:.4f}".format(payload_out.quat_k)
-    quat_real = "{:.4f}".format(payload_out.quat_real)
-    accuracy = "{:d}".format(payload_out.accuracy)
-
-    packet_table.add_row(['Out', timestamp, request, response, id_value, quat_i, quat_j, quat_k, quat_real, accuracy])
-
-    timestamp = "{:.4f}".format((current_time - prev_time)/1000)
-    request = Requests(payload_in.request).name
-    response = Responses(payload_in.response).name
-    id_value = "0x{:X}".format(payload_in.id)
-    quat_i = "{:.4f}".format(payload_in.quat_i)
-    quat_j = "{:.4f}".format(payload_in.quat_j)
-    quat_k = "{:.4f}".format(payload_in.quat_k)
-    quat_real = "{:.4f}".format(payload_in.quat_real)
-    accuracy = "{:d}".format(payload_in.accuracy)
-
-    packet_table.add_row(['In', timestamp, request, response, id_value, quat_i, quat_j, quat_k, quat_real, accuracy])
-    
-    print(colored(packet_table, 'white'))
-
-def send_tare(server_address): 
-    send_packet(Requests.TARE_REQ.value, Responses.NULL_RESP.value, server_address)
-    receive_packet()
-
-
-
-def send_calibration(server_address): 
-    response = Responses.BUSY_RESP.value
-    prev_time = 0
-
-    #send calibration requests until success or failure response received back from esp32
-    while (response != Responses.SUCESS_RESP.value) and (response != Responses.FAILURE_RESP.value):
-        try:
-            print(colored('Awaiting sucess/fail response.', 'yellow'))
-
-            payload_out = send_packet(Requests.CALIBRATE_REQ.value, Responses.NULL_RESP.value, server_address)
-            payload_in = receive_packet(); 
-
-            response = payload_in.response
-
-            # print the received data
-            print_packet(payload_in, payload_out, payload_in.time_stamp, prev_time)
-            prev_time = payload_in.time_stamp
-
-            if (response == Responses.SUCESS_RESP.value):
-                print(colored('Sucessful calibration.', 'green'))
-            elif (response == Responses.FAILURE_RESP.value):
-                print(colored('Failed calibration.', 'red'))
-
-        except socket.timeout:
-                print(colored('ERROR: Timeout occurred, resending request...', 'red'))
-                continue
-    
-    #send sampling request with affirmative response until sampling response receieved 
-    while response != Responses.SAMPLING_RESP.value:
-        try:
-            print(colored('Awaiting sampling response.', 'yellow'))
-            payload_out = send_packet(Requests.SAMPLE_REQ.value, Responses.AFFIRMATIVE_RESP.value, server_address)
-            payload_in = receive_packet()
-
-            response = payload_in.response
-            print_packet(payload_in, payload_out, payload_in.time_stamp, prev_time)
-            prev_time = payload_in.time_stamp
-
-        except socket.timeout:
-                print(colored('ERROR: Timeout occurred, resending request...', 'red'))
-                continue
-
-
+# Initialize lists to store quaternion data
+time_stamps = []
+quat_i_data = []
+quat_j_data = []
+quat_k_data = []
+quat_data = []
 prev_time = 0
 
-while True:
-    try:
-        payload_out = send_packet(Requests.SAMPLE_REQ.value, Responses.NULL_RESP.value, server_address_1)
-        payload_in = receive_packet()
+# Enable interactive mode for real-time plotting
+plt.ion()
+fig, ax = plt.subplots(figsize=(8, 6))  # Create a single subplot
+lines_i, = ax.plot([], [], label='quat_i')  # Line for quat_i
+lines_j, = ax.plot([], [], label='quat_j')  # Line for quat_j
+lines_k, = ax.plot([], [], label='quat_k')  # Line for quat_k
+lines_real, = ax.plot([], [], label='quat_real')  # Line for quat_real
+ax.legend()
+ax.set_xlabel('Time Stamp')
+ax.set_ylabel('Quaternion Value')
+ax.set_title('Quaternion Data Over Time')
 
-        # print the received data
+plt.tight_layout()
+plt.show()
+ax.set_xlim(0, 10000)
+ax.set_ylim(-1, 1)
+total_time = 0
+
+
+def keyboard_input_thread():
+    global quit_flag
+    global send_tare_flag
+    global send_calibration_flag
+
+    while not quit_flag.is_set():
+        time.sleep(0.070)
+
+        # check for keyboard input
+        if keyboard.is_pressed('q'):
+            quit_flag.set()
+
+        if keyboard.is_pressed('t'):
+            send_tare_flag.set()
+
+        if keyboard.is_pressed('c'):
+            send_calibration_flag.set()
+
+
+keyboard_input_thread_hdl = threading.Thread(target = keyboard_input_thread)
+keyboard_input_thread_hdl.start()
+
+while not quit_flag.is_set():
+    try:
+        if total_time > 10000:
+            total_time = 0
+            time_stamps.clear()
+            quat_data.clear()
+
+        payload_out, payload_in = send_sample_req(data_stream, server_address)
+        # Append received quaternion data to lists
+        total_time = total_time + (payload_in.time_stamp/1000 - prev_time/1000)
+        time_stamps.append(total_time)
+        quat_data.append((payload_in.quat_i, payload_in.quat_j, payload_in.quat_k, payload_in.quat_real))
+
+        # Print the received data
         print_packet(payload_in, payload_out, payload_in.time_stamp, prev_time)
-        #print(str(prev_time) + " " + str(payload_in.time_stamp) + '\n')
         prev_time = payload_in.time_stamp
 
-  
+        # Update the plot with new data for each component
+        quat_data_array = np.array(quat_data)
+        lines_i.set_data(time_stamps, quat_data_array[:, 0])  # Update quat_i data
+        lines_j.set_data(time_stamps, quat_data_array[:, 1])  # Update quat_j data
+        lines_k.set_data(time_stamps, quat_data_array[:, 2])  # Update quat_k data
+        lines_real.set_data(time_stamps, quat_data_array[:, 3])  # Update quat_real data
 
-    
+        # Draw the plot
+        plt.draw()
+        plt.pause(0.001)  # Pause to allow time for the plot to update
+
     except socket.timeout:
         print(colored('ERROR: Timeout occurred, resending request...', 'red'))
         continue
-    
-    #quit 
-    if keyboard.is_pressed('q'):
-        print(colored('Quitting, socket shutting down.', 'magenta'))
-        data_stream.close()
-        break  # Exit the loop if 'q' is pressed
 
-    #tare imu  
-    if keyboard.is_pressed('t'):
+    # Tare IMU
+    if send_tare_flag.is_set():
         print(colored('Taring', 'magenta'))
         time.sleep(0.1)
-        send_tare(server_address_1)
+        send_tare_req(data_stream, server_address)
+        send_tare_flag.clear()
 
-    #calibrate imu
-    if keyboard.is_pressed('c'):
+    # Calibrate IMU
+    if send_calibration_flag.is_set():
         print(colored('Calibrating', 'magenta'))
-        send_calibration(server_address_1)
+        send_calibration_req(data_stream, server_address)
         time.sleep(0.1)
+        send_calibration_flag.clear()
+
+keyboard_input_thread_hdl.join()
+data_stream.close()
