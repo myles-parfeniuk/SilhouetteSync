bdc925e91ad183dde250c1afcab60c014b8f3e4e
Kieran MacCarron <146384447+kmac333@users.noreply.github.com>
2024-03-07 16:11:45 -0800

    Unity Copy

---
+++ UnityApp copy/SilSync/Assets/Code/IncreaseButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+ 
+public class ScoreScript : MonoBehaviour {
+ 
+    public TextMeshProUGUI scoreText;
+    private int score;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+        updateScoreUI();
+    }
+    public void addScore(int xAmount)
+    {
+        score = int.Parse(scoreText.text);
+        score += xAmount;
+        updateScoreUI();
+    }
+ 
+    public void ReduceScore(int yAmount)
+    {
+        score = int.Parse(scoreText.text);
+        score -= yAmount;
+        updateScoreUI();
+    }
+ 
+    void updateScoreUI()
+    {
+        scoreText.text = score.ToString();
+    }
+ 
+ 
+}
